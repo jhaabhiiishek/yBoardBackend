f337eb890f6308d26726b0b2085dd189fecdc7e6
Abhishek Jha <abhi10092003@gmail.com>
2025-11-22 15:22:07 +0530

    production changes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json())
 app.use(cors({
-	origin:"http://localhost:5173",
+	origin:"https://yboards.netlify.app/",
 	credentials:true
 }))
 app.use(cookieParser());
@@ -23,7 +23,7 @@ app.use(cookieParser());
 const server = createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:5173",
+		origin: "https://yboards.netlify.app/",
 		methods: ["GET", "POST"],
 		credentials: true
 	}
